e36e9a64cb5a276cde90e51f4ff0e06f0dbdcd26
khfiz <108290770+khfiz@users.noreply.github.com>
2022-06-27 23:40:10 +0700

    Add files via upload

---
+++ rental-mobil/views/_partials/footer.php
@@ -0,0 +1,8 @@
+<footer class="sticky-footer bg-green">
+	<div class="container my-auto">
+	    <div class="copyright text-center my-auto">
+	    <span>
+	    <a href="https://github.com/nugrohoff" target="_blank">Khafiz Sholata</a> &copy; Aplikasi Rental Mobil <?= date('Y') ?></span>
+	    </div>
+	</div>
+</footer>
